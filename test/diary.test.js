f417ddcb2e733e4c90e03b4004685cf2a36f6195
thoughtsunificator <85041649+thoughtsunificator@users.noreply.github.com>
2021-09-18 16:41:42 +0200

    mocha

--- test/diary.test.js
+++ test/diary.test.js
@@ -1,11 +1,12 @@
 import path from 'path'
+import assert from "assert"
 import puppeteer from 'puppeteer'
 import { fileURLToPath  } from "url"
 
 let browser
 let page
 
-export async function setUp(done) {
+beforeEach(async () => {
 	try {
 		browser = await puppeteer.launch({
 			headless: true
@@ -13,22 +14,21 @@ export async function setUp(done) {
 		page = await browser.newPage()
 		await page.goto(`http://localhost:8080`)
 		await page.waitForSelector('div')
-		done()
 	} catch(error) {
 		console.error(error)
 	}
-}
+})
 
-export async function tearDown(done) {
+afterEach(async () => {
 	await browser.close()
-	done()
-}
+})
 
-export async function diary(test) {
-	test.expect(1)
+it('should return domodel-diary', async function() {
 	const title = await page.title()
-	test.strictEqual(title, "domodel-diary")
-	const x = await page.evaluate(() => {
-		return document.body.innerHTML
-	})
-}
+	assert.equal(title, "domodel-diary")
+})
+
+it('should have #auth', async function() {
+	const authNode = await page.evaluate(() => document.querySelector("#auth"))
+	assert.notEqual(authNode, null)
+})
